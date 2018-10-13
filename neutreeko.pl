6b65b23c91a91039cbe3caec68a98acdc43bf1b1
Bernardo Barbosa <up201503477@fe.up.pt>
2018-10-13 20:01:19 +0100

    display_game WIP :construction:

--- neutreeko.pl
+++ neutreeko.pl
@@ -7,3 +7,18 @@ board([
 [0,0,'branca',0,0],
 [0,'preta',0,'preta',0]
 ]).
+
+display_game(Board, Player) :- display_board(Board).
+
+display_board([]).
+display_board([L|T]):- display_line(L), nl,
+                        display_board(T).
+
+display_line([]).
+display_line([C|L]) :- display_cell(C),
+                        display_line(L).
+
+display_cell([]).
+display_cell(0) :- write('-').
+display_cell('branca') :- write('B').
+display_cell('preta') :- write('P').
